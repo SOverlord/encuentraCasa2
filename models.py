a7f8ab1fac7b57d8609950a9d2f51cfc8f9a6207
SpeedStream <aesc91@hotmail.com>
2016-06-13 01:07:05 -0500

    V5.5

--- models.py
+++ models.py
@@ -4,22 +4,38 @@ from google.appengine.ext import blobstore
 class Usuario(db.Model):
     str_email = db.StringProperty()
     str_password = db.StringProperty()
-#Agregar un usuario predeterminado
 
+i = 0
+if i == 0:
+    newUsr = Usuario(str_email="def@def.com", str_password="1")
+    newUsr.put()
+    i=i+1
 
 class Lugar(db.Model):
     #int_LugarID = ID autogenerado por GAE
     int_publicarPension = db.IntegerProperty(default=0)
     str_urlFotoPerfil = db.StringProperty(default = "0")
     str_nombrePension = db.StringProperty()
-    str_descripcion = db.StringProperty()
+    txt_descripcion = db.TextProperty()
     str_nombreArrendador = db.StringProperty()
+    str_tipoLugar = db.StringProperty()
     str_ciudad = db.StringProperty()
     str_direccion = db.StringProperty()
     str_cp = db.StringProperty()
     str_tipo = db.StringProperty()
     str_telefonoCasa = db.StringProperty()
     str_telefonoCelular = db.StringProperty()
+    str_email = db.StringProperty()
+
+    str_luz = db.StringProperty(default="False")
+    str_internet = db.StringProperty(default="False")
+    str_cable = db.StringProperty(default="False")
+    str_telefono = db.StringProperty(default="False")
+    str_lavanderia = db.StringProperty(default="False")
+    str_comidas = db.StringProperty(default="False")
+    str_aguaCaliente = db.StringProperty(default="False")
+    str_amueblado = db.StringProperty(default="False")
+    str_limpieza = db.StringProperty(default="False")
 
 class UserUpload(db.Model):
     int_LugarKey = db.IntegerProperty(default=0)
--- registrarNuevaPension.py
+++ registrarNuevaPension.py
@@ -15,12 +15,23 @@ class registrarNuevaPension(webapp2.RequestHandler):
         l_ciudad = self.request.get('ciudad')
         l_direccion = self.request.get('direccion')
         l_cp = self.request.get('cp')
+        l_email = self.request.get('email')
         l_telCasa = self.request.get('telefonoCasa')
         l_telCel = self.request.get('telefonoCelular')
         l_tipo = self.request.get('tipo')
+        l_tipoLugar = self.request.get('tipoLugar')
         l_descripcion = self.request.get('descripcion')
-        
-        
+
+        l_luz = self.request.get('servicio1', default_value='False')
+        l_internet = self.request.get('servicio2', default_value='False')
+        l_cable = self.request.get('servicio3', default_value='False')
+        l_telefono = self.request.get('servicio4', default_value='False')
+        l_lavanderia = self.request.get('servicio5', default_value='False')
+        l_comida = self.request.get('servicio6', default_value='False')
+        l_aguaCaliente = self.request.get('servicio7', default_value='False')
+        l_amueblado = self.request.get('servicio8', default_value='False')
+        l_limpieza = self.request.get('servicio9', default_value='False')
+
         #Almacenamos el contenido de las variables locales en la base de datos
         pension = models.Lugar()
         pension.str_nombrePension = l_nombrePension
@@ -28,13 +39,25 @@ class registrarNuevaPension(webapp2.RequestHandler):
         pension.str_ciudad = l_ciudad
         pension.str_direccion = l_direccion
         pension.str_cp = l_cp
-        pension.str_tipo = l_tipo
+        pension.str_email = l_email
         pension.str_telefonoCasa = l_telCasa
         pension.str_telefonoCelular = l_telCel
-        pension.str_descripcion = l_descripcion
+        pension.str_tipo = l_tipo
+        pension.str_tipoLugar = l_tipoLugar
+        pension.txt_descripcion = l_descripcion
+
+        pension.str_luz = l_luz
+        pension.str_internet = l_internet
+        pension.str_cable = l_cable
+        pension.str_telefono = l_telefono
+        pension.str_lavanderia = l_lavanderia
+        pension.str_comidas = l_comida
+        pension.str_aguaCaliente = l_aguaCaliente
+        pension.str_amueblado = l_amueblado
+        pension.str_limpieza = l_limpieza
         
         pension.str_urlFotoPerfil = "0" #significa que no tiene imagen
-        #colocamos esas variables en el objeto usuario
+        #colocamos esas variables en el objeto pension
         pension.put()
 
         #si no metio un correo valido muestra un mensaje un mensaje
